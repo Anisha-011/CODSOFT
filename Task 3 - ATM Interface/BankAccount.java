5756ef0bd87ce663ddc413ac8e06ce9dfccf03e1
Anisha-011 <anishapaikaray2004@gmail.com>
2025-06-29 23:08:14 +0530

    Create BankAccount.java

---
+++ Task 3 - ATM Interface/BankAccount.java
@@ -0,0 +1,31 @@
+public class BankAccount {
+    private double balance;
+
+    public BankAccount(double initialBalance) {
+        balance = initialBalance;
+    }
+
+    public void deposit(double amount) {
+        if (amount > 0) {
+            balance += amount;
+            System.out.println("Amount deposited successfully.");
+        } else {
+            System.out.println("Invalid deposit amount.");
+        }
+    }
+
+    public void withdraw(double amount) {
+        if (amount > balance) {
+            System.out.println("Insufficient balance.");
+        } else if (amount <= 0) {
+            System.out.println("Invalid withdrawal amount.");
+        } else {
+            balance -= amount;
+            System.out.println("Withdrawal successful.");
+        }
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+}
